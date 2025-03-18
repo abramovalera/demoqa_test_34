b99b3df45f0f02fdeba926a89b2b8129091da2b7
v <abramovaera15@mail.ru>
2025-03-18 20:48:45 +0300

    Удалена папка .idea из репозитория Переименован метод на successLoginTest Добавлена загрузка картинки через uploadFromClasspath() Улучшен поиск штата и города через контейнер Добавлены проверки всех данных в итоговой таблице Добавлен скролл вниз после ввода адреса

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,36 +1,47 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.selected;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
     @Test
-    void successLogin() {
-        Configuration.holdBrowserOpen = false;
+    void successLoginTest() {
+        Configuration.holdBrowserOpen = true;
 
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue("Valery");
         $("#lastName").setValue("Abram");
         $("#userEmail").setValue("test@mail.com");
         $("#gender-radio-1").parent().click();
-        $("#gender-radio-1").shouldBe(selected);
+        $("#gender-radio-1").shouldBe(Condition.selected);
         $("#userNumber").setValue("8900123450");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1995");
-        $(".react-datepicker__day--001").click();
+        $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("English").pressEnter();
+        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);"); // скрол
         $("#hobbies-checkbox-1").parent().click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/img/cat.jpg"));
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("ул. Боброва д. 001");
         $("#state").click();
-        $(byText("Haryana")).click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
         $("#city").click();
-        $(byText("Karnal")).click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+        $(".table-responsive").shouldHave(
+                Condition.text("Valery Abram"),
+                Condition.text("test@mail.com"),
+                Condition.text("Male"),
+                Condition.text("8900123450"),
+                Condition.text("01 July,1995"),
+                Condition.text("English"),
+                Condition.text("Sports"),
+                Condition.text("cat.jpg"),
+                Condition.text("ул. Боброва д. 001"),
+                Condition.text("Haryana Karnal")
+        );
     }
 }
