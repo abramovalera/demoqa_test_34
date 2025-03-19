25aca76b21972a7c5f148022046de99d6bac2865
v <abramovaera15@mail.ru>
2025-03-19 11:51:48 +0300

    upd добавил откючение банеров + проверка в конце кадого значения

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -11,6 +11,8 @@ public class PracticeFormTest {
         Configuration.holdBrowserOpen = true;
 
         open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Valery");
         $("#lastName").setValue("Abram");
         $("#userEmail").setValue("test@mail.com");
@@ -22,7 +24,6 @@ public class PracticeFormTest {
         $(".react-datepicker__year-select").selectOption("1995");
         $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("English").pressEnter();
-        executeJavaScript("window.scrollTo(0, document.body.scrollHeight);"); // скрол
         $("#hobbies-checkbox-1").parent().click();
         $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("ул. Боброва д. 001");
@@ -31,17 +32,16 @@ public class PracticeFormTest {
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Karnal")).click();
         $("#submit").click();
-        $(".table-responsive").shouldHave(
-                Condition.text("Valery Abram"),
-                Condition.text("test@mail.com"),
-                Condition.text("Male"),
-                Condition.text("8900123450"),
-                Condition.text("01 July,1995"),
-                Condition.text("English"),
-                Condition.text("Sports"),
-                Condition.text("cat.jpg"),
-                Condition.text("ул. Боброва д. 001"),
-                Condition.text("Haryana Karnal")
-        );
+        $(".table-responsive").shouldBe(Condition.visible);
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(Condition.text("Valery Abram"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(Condition.text("test@mail.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(Condition.text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(Condition.text("8900123450"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(Condition.text("01 July,1995"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(Condition.text("English"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(Condition.text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(Condition.text("cat.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(Condition.text("ул. Боброва д. 001"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(Condition.text("Haryana Karnal"));
     }
 }
